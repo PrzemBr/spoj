8388ac4f36ba119a12c05eb1fe7ef883467ad0fd
Przemyslaw Bryda <przemyslaw.bryda@gmail.com>
2019-10-10 13:53:34 +0200

    added treasureHunt class

--- src/main/java/SKARBFI/treasureHunt.java
+++ src/main/java/SKARBFI/treasureHunt.java
@@ -1,4 +1,16 @@
 package SKARBFI;
 
 public class treasureHunt {
+    private int baseVertical = 0;
+    private int baseHorizon = 0;
+
+    public int changeVertical(int vertical) {
+        int verticalOutpu = baseVertical + vertical;
+        return verticalOutpu;
+    }
+
+    public int changeHorizon(int horizon) {
+        int horizonOutput = baseHorizon + horizon;
+        return horizonOutput;
+    }
 }
